e6faf4d0e3051f600bbdf40d038a87d92d84e6fd
Mati Joseph <crucialniccur@gmail.com>
2025-05-24 16:35:12 +0300

    define the Students class with a tablename and table arguments

--- app/models.py
+++ app/models.py
@@ -8,3 +8,12 @@ from datetime import datetime
 engine = create_engine('sqlite:///migrations_test.db')
 
 Base = declarative_base()
+
+
+class Student(Base):
+    __tablename__ = 'students'
+    __table_args__ = (
+        UniqueConstraint('email', name='unique_email'),
+        CheckConstraint('grade BETWEEN 1 and 12',
+                        name='grade_between_1_and_12')
+    )
