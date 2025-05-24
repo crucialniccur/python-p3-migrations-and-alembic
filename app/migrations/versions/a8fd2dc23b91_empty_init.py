bd3d702b62aacaa1031e6b2cb079953ac44d4a5b
Mati Joseph <crucialniccur@gmail.com>
2025-05-24 16:20:40 +0300

    first alembic version created via revision -m

---
+++ app/migrations/versions/a8fd2dc23b91_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty init
+
+Revision ID: a8fd2dc23b91
+Revises: 
+Create Date: 2025-05-24 16:18:12.179838
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a8fd2dc23b91'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
