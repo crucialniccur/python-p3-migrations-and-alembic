afbea10c01194999107efe224893e553026660e0
Mati Joseph <crucialniccur@gmail.com>
2025-05-24 16:31:35 +0300

    add imports in the models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, desc
+from sqlalchemy import (CheckConstraint, UniqueConstraint,
+                        Column, DateTime, Integer, String)
 
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
 engine = create_engine('sqlite:///migrations_test.db')
 
