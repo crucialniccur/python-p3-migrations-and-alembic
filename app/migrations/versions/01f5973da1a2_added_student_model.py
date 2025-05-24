ac0a74706d3c6b126d429219bf001de54e20a9a5
Mati Joseph <crucialniccur@gmail.com>
2025-05-24 16:39:53 +0300

    alembic revision version file generated

---
+++ app/migrations/versions/01f5973da1a2_added_student_model.py
@@ -0,0 +1,24 @@
+"""Added Student model
+
+Revision ID: 01f5973da1a2
+Revises: a8fd2dc23b91
+Create Date: 2025-05-24 16:39:22.459226
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '01f5973da1a2'
+down_revision = 'a8fd2dc23b91'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
